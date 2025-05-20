c53ab2f7f0988b42e234ad36e9ebd93a99d74eb2
VIVEK PRAKASH <prematprakash@gmail.com>
2025-05-20 10:44:57 +0530

    Add files via upload

---
+++ ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "system update ho raha hai "
+sudo apt update
+
+echo " open ssh install hora h"
+sudo apt install openssh-server
+
+echo "enabling the ssh"
+sudo systemctl enable ssh
+
+echo "starting the ssh"
+sudo systemctl start ssh
